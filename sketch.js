9d6107a3129fc4f2c9aba23acd6a80e4227d484a
Skyedoddles <siddhi.bhavya.sod23@anu.edu.in>
2024-07-22 06:35:03 +0530

    it is a brush

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  colorMode(HSB);
+  
+}
+function draw() {
+
+  //if (mouseIsPressed === true) {
+  //  fill(214, 211, 15);
+ // } else {
+ //   fill(214, 17, 20);
+//  }
+  // noStroke();
+
+ // heart(mouseX, mouseY, 20);
+  if (mouseIsPressed) {
+    brush()
+  }
+}
+
+function brush() {
+  // set the color!!!!
+  //multicolor brush yay
+  fill(frameCount % 360,100,100);
+  noStroke()
+  
+  heart(mouseX,mouseY,20)
+}
+
+ 
+function heart(x, y, size) {
+  beginShape();
+  vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
+}
